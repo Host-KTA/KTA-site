ba7423f66e48e14e563b74772fc87e9ef666eed1
Host-KTA <koffeezip@gmail.com>
2026-07-21 22:19:22 +0900

    Create categories.js

---
+++ categories.js
@@ -0,0 +1,27 @@
+const categories = [
+    {
+        id: "news",
+        name: "변경 사항",
+        page: "update.html"
+    },
+    {
+        id: "coupon",
+        name: "쿠폰 정보",
+        page: "coupon.html"
+    },
+    {
+        id: "gameinfo",
+        name: "게임 설명",
+        page: "game rules.html"
+    },
+    {
+        id: "episode",
+        name: "에피소드",
+        page: "episode.html"
+    },
+    {
+        id: "IMO",
+        name: "설정 해석",
+        page: "IMO.html"
+    }
+];
