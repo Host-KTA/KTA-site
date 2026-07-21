9e72365c3214fdb7ac8e05ade3c6dd9dbd24b86e
Host-KTA <koffeezip@gmail.com>
2026-07-21 23:09:54 +0900

    Update admin.js

--- admin.js
+++ admin.js
@@ -21,6 +21,7 @@ document.getElementById("category").value;
 
 const selectedCategory =
 categories.find(category => category.id === categoryId);
+    console.log(selectedCategory);
 
 const finalFilename = filename || title.replace(/\s+/g, "_");
 
