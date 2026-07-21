dbcb74571fa30fc089c68f4b005bc07f629c84c8
Host-KTA <koffeezip@gmail.com>
2026-07-22 00:31:04 +0900

    Update categories.js

--- categories.js
+++ categories.js
@@ -1,42 +1,32 @@
 const categories = [
-
     {
         id: "update",
-        name: "변경 사항",
-        page: "update/update.html",
-        folder: "update"
+        name: "변경 사항(Update)",
+        folder: "update",
+        page: "update.html"
     },
-
-
     {
         id: "coupon",
-        name: "쿠폰 정보",
-        page: "coupon/coupon.html",
-        folder: "coupon"
+        name: "쿠폰 정보(Coupon)",
+        folder: "coupon",
+        page: "coupon.html"
     },
-
-
     {
         id: "gamerule",
-        name: "게임 설명",
-        page: "gamerule/gamerule.html",
-        folder: "gamerule"
+        name: "게임 설명(Rules)",
+        folder: "gamerule",
+        page: "gamerule.html"
     },
-
-
     {
         id: "episode",
-        name: "에피소드",
-        page: "episode/episode.html",
-        folder: "episode"
+        name: "에피소드(Episodes)",
+        folder: "episode",
+        page: "episode.html"
     },
-
-
     {
         id: "IMO",
-        name: "설정 해석",
-        page: "IMO/IMO.html",
-        folder: "IMO"
+        name: "설정 해석(IMO)",
+        folder: "IMO",
+        page: "IMO.html"
     }
-
 ];
